b02749ac7f2380909a78040ab49828d05ef157f2
Shao-Chung Chen <dannvix@gmail.com>
2014-10-08 22:08:42 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# yyets-subdl
+YYeTs (人人影视) 雙語字幕下載器，小小工具、大大方便！
+
+
+## 一鍵下載字幕
+* 給定關鍵字，自動尋找 YYeTs 相關字幕
+* 下載完成、自動解壓縮，萃取「…繁體&英文.ass」
+
+
+## 相依性
+* Python [`requests`](http://docs.python-requests.org/en/latest/) 函式庫
+* `curl` 工具
+
+
+## 使用方式
+* 找個地方放，例如 `/usr/local/bin`
+* 命令列執行 `yyets-dl <關鍵字>`
+
+
+## MIT License
+Copyright (c) 2014 Shao-Chung Chen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ yyets-subdl.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# yyets-subdl.py - command-line downloader for YYeTs subtitles
+
+import os
+import sys
+import shutil
+import zipfile
+
+# pip install requests
+import requests
+
+
+def query(keyword):
+    request_url = 'http://www.yyets.com/php/search/api'
+    request_params = dict(keyword=keyword)
+    response = requests.get(request_url, params=request_params)
+    if response.status_code == requests.codes.ok:
+        items = response.json()['data']
+        choices = [x for x in items if x['type'] == 'subtitle']
+        return choices
+    else:
+        raise Exception('HTTP status_code=%s' % response.status_code)
+    return []
+
+
+def ask(choices):
+    for idx, choice in enumerate(choices):
+        print '%2d. %s' % (idx+1, choice['title'])
+        print '    %s' % choice['version']
+    answers = raw_input('What items do you want? (seperated by commas) [1] ')
+    if answers: return map(lambda x: int(x)-1, answers.split(r','))
+    else: return [0]
+
+
+def download(items):
+    files = []
+    for item in items:
+        # item_url = 'http://www.yyets.com/%s/%s' % (item['type'], item['itemid'])
+        download_url = 'http://www.yyets.com/%s/index/download?id=%s' % (item['type'], item['itemid'])
+        download_filename = '%s.zip' % (item['version'].split(';')[0] or item['title'])
+        command_line = "curl -L '%s' > '%s'" % (download_url, download_filename)
+        print command_line
+        os.system(command_line.encode('utf-8'))
+        files.append(download_filename)
+    return files
+
+
+def extract(files):
+    for filepath in files:
+        print 'Examining %s' % filepath
+        if not 'zip' in os.path.splitext(filepath)[1].lower():
+            continue
+        extract_success = False
+        with zipfile.ZipFile(filepath) as archive:
+            for item in archive.namelist():
+                filename = os.path.split(item)[1].decode('gb2312').encode('utf-8')
+                extension = os.path.splitext(filename)[1].lower()
+
+                found = False
+                target_lang = u'繁体&英文'.encode('utf-8')
+                if ('ass' in extension) and (target_lang in filename):
+                    # zipfile.extract() makes filename escaped with '%xx'
+                    with archive.open(item, 'r') as infile, open(filename, 'wb') as outfile:
+                        shutil.copyfileobj(infile, outfile)
+                        extract_success = True
+                    print 'Extracted %s' % filename
+        if extract_success:
+            os.remove(filepath)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print 'Usage: %s <keyword>' % sys.argv[0]
+        print "Example: %s 'Person of Interest S04E01'" % sys.argv[0]
+        sys.exit(1)
+    keyword = sys.argv[1]
+    choices = query(keyword)
+    chosen_ids = ask(choices)
+    chosens = [choices[idx] for idx in chosen_ids]
+    files = download(chosens)
+    extract(files)
